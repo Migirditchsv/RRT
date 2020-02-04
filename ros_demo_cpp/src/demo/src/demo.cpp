d032f16c67117a97b68447938eb268d62e6e6127
Sam <migirditch@gmail.com>
2020-02-04 00:21:46 -0500

    adj graph added

--- ros_demo_cpp/src/demo/src/demo.cpp
+++ ros_demo_cpp/src/demo/src/demo.cpp
@@ -30,6 +30,76 @@ const int SEED = 59073451;
 std::random_device                  rand_dev;
 std::mt19937                        generator(rand_dev());
 
+// This class represents a directed graph using 
+// adjacency list representation 
+class Graph  // graph and bfs stolen from geeks 4 geeks
+{ 
+    int V;    // No. of vertices 
+  
+    // Pointer to an array containing adjacency 
+    // lists 
+    list<int> *adj;    
+public: 
+    Graph(int V);  // Constructor 
+  
+    // function to add an edge to graph 
+    void addEdge(int v, int w);  
+  
+    // prints BFS traversal from a given source s 
+    void BFS(int s);   
+}; 
+
+  
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list<int>[V]; 
+} 
+  
+void Graph::addEdge(int v, int w) 
+{ 
+    adj[v].push_back(w); // Add w to v’s list. 
+} 
+  
+void Graph::BFS(int s) 
+{ 
+    // Mark all the vertices as not visited 
+    bool *visited = new bool[V]; 
+    for(int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    // Create a queue for BFS 
+    list<int> queue; 
+  
+    // Mark the current node as visited and enqueue it 
+    visited[s] = true; 
+    queue.push_back(s); 
+  
+    // 'i' will be used to get all adjacent 
+    // vertices of a vertex 
+    list<int>::iterator i; 
+  
+    while(!queue.empty()) 
+    { 
+        // Dequeue a vertex from queue and print it 
+        s = queue.front(); 
+        cout << s << " "; 
+        queue.pop_front(); 
+  
+        // Get all adjacent vertices of the dequeued 
+        // vertex s. If a adjacent has not been visited,  
+        // then mark it visited and enqueue it 
+        for (i = adj[s].begin(); i != adj[s].end(); ++i) 
+        { 
+            if (!visited[*i]) 
+            { 
+                visited[*i] = true; 
+                queue.push_back(*i); 
+            } 
+        } 
+    } 
+} 
+
 struct rrtInfo
 {
   std::vector<geometry_msgs::Point> pointList;
@@ -232,7 +302,7 @@ geometry_msgs::Point clippingCheck(geometry_msgs::Point a, geometry_msgs::Point 
     collision = collisionTest(&check, obj);
     if( collision ==1 )
     {
-      std::cout << "clippingCheck| edge intersects. Returning intermediatept at ("<<check.x<<","<<check.y<<")\n" <<fflush;
+      //std::cout << "clippingCheck| edge intersects. Returning intermediatept at ("<<check.x<<","<<check.y<<")\n" <<fflush;
       return(check);
     }
     // grow the check vector
@@ -318,10 +388,11 @@ void rrtBuild(geometry_msgs::Point startPoint, geometry_msgs::Point stopPoint, v
   rrtResult.pointList = pointList;
   rrtResult.edgeList = edgeList;
 
-  for( int i =0; i<8; i++)
-  {
-  std::cout<<"rrtBuild| fuckup["<<i<<"]: "<<fuckup[i]<<std::endl;
-  }
+  // translate into adj graph for more efficient bfs
+  Graph adjGraph( sizeof(pointList) );
+  
+
+
 
 }
 
