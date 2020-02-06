7da5c892f26edd6512a2cd6e7e7c218365890887
Sam <migirditch@gmail.com>
2020-02-06 14:07:35 -0500

    radn setup

--- ros_demo_cpp/src/demo/src/rrt.hpp
+++ ros_demo_cpp/src/demo/src/rrt.hpp
@@ -8,9 +8,45 @@
 #ifndef RAPIDRANDOMTREE
 #define RAPIDRANDOMTREE
 
+// Include
+#include <iostream> // output
+#include <random> //prng for random point placement
+
+// Use namespaces
+using namespace std;
+
+// GLobal Objects
+
+// Global functions
 int testFunction(int i)
 {
     return(i);
+}
+
+// Structs
+struct point
+{
+    public:
+
+    // Public Vars
+    double x,y; // position
+    int index, parentIndex; // index for incluison in vector of points & index of parent. 
+    point *parent; // pointer to unique parent
+
+    // Public Functions
+    void randomPlacement(visualization_msgs::Marker obst);
+    int nearestNeighbor(vector<point> tree); // return index of nearest neighbor in tree
+
+    private:
+
+    // Private Vars
+
+    // Private fxns
+    void validPoint(visualization_msgs::Marker obst);
+    double pointDistance(point target);
+
+
+
 }
 
 #endif
